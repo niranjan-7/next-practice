218ea0aac96114092f342c3e74d29cfa979d6772
niranjan-7 <86029534+niranjan-7@users.noreply.github.com>
2023-06-04 09:37:19 +0530

    First Commit

---
+++ next_practice/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
+    distDir: 'build',
+  };
+  
+  module.exports = nextConfig;
+  
